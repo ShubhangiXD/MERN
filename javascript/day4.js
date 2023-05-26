842fb02086278df0e227b9fbc57ba59967deb2ed
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-05-26 11:47:29 +0530

    modified:   javascript/day4.js

--- javascript/day4.js
+++ javascript/day4.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+//reassigning values
+var myName = "Shubhangi"
+console.log(myName)
+myName = "Shoofus"
+console.log(myName)
+
+let yourName = "Renyxx"
+console.log(yourName)
+//let yourName = "Reny" //will give an error
+yourName = "Reny"
+console.log(yourName)
+
+//fixed variables
+const pi = 3.14
+console.log(pi)
